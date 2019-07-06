6d7109bba644e2027d0bd89e2d5d6c5dddea6984
Mutlu Polatcan <mutlu.polatcan@cci.com.tr>
2019-07-06 14:11:51 +0300

    mahout versions updated

--- build.sh
+++ build.sh
@@ -3,18 +3,14 @@
 HADOOP_VERSIONS=(
     "3.2.0"
     "3.1.2"
-    "3.1.1"
     "3.0.3"
-    "3.0.2"
     "2.9.1"
-    "2.9.0"
     "2.8.4"
     "2.7.6"
     "2.6.5"
 )
 
 MAHOUT_VERSIONS=(
-    "0.14.0"
     "0.13.0"
     "0.12.2"
     "0.11.2"
@@ -31,7 +27,7 @@ DISTS=(
 function build_image() {
     sudo docker build -q -t mpolatcan/mahout:$1-$2-hadoop-$3 --build-arg MAHOUT_VERSION=$2 --build-arg HADOOP_VERSION=$3 ./$1/
 	sudo docker push mpolatcan/mahout:$1-$2-hadoop-$3
-	sudo docker rmi $(sudo docker images -q)
+	sudo docker rmi mpolatcan/mahout:$1-$2-hadoop-$3
 }
 
 for MAHOUT_VERSION in ${MAHOUT_VERSIONS[@]}; do
