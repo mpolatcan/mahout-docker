0432098168cbb424d5378df6311526526b462412
mpolatcan <mutlupolatcan@gmail.com>
2019-10-24 00:48:26 +0300

    mahout docker image refactored

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 HADOOP_VERSIONS=(
     "3.2.1"
-    "3.1.2"
+    "3.1.3"
     "2.9.2"
     "2.8.5"
     "2.7.7"
@@ -18,15 +18,13 @@ JAVA_VERSIONS=(
     "8"
 )
 
-# $1: MAHOUT_VERSION
-# $2: HADOOP_VERSION
-# $3: JAVA_VERSION
 function build_image() {
   sudo docker build -q -t mpolatcan/mahout:$1-hadoop-$2-java$3 --build-arg MAHOUT_VERSION=$1 --build-arg HADOOP_VERSION=$2 --build-arg JAVA_VERSION=$3 ./src/
 	sudo docker push mpolatcan/mahout:$1-hadoop-$2-java$3
 	sudo docker rmi mpolatcan/mahout:$1-hadoop-$2-java$3
 }
 
+# Build Mahout images for each Hadoop and Java versions
 for MAHOUT_VERSION in ${MAHOUT_VERSIONS[@]}; do
     for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
         for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
